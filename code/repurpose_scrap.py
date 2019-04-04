19ba06f2b6e1e7b4dc44691a7a04fe575d2a2414
Benardi <benardinunes@gmail.com>
2019-04-04 01:21:51 -0300

    Add utility procedures to help enforce csv format
    Add utility procedures to help remove commas aside from separators.

--- code/repurpose_scrap.py
+++ code/repurpose_scrap.py
@@ -12,17 +12,35 @@ def unpack_json(path):
     
     return data
 
+
 def conform_date(date):
    time = parser.parse(date)
    formatted = time.strftime('%d/%m/%Y %H:%M:%S')
    return formatted
 
+
+def conform_text(text):
+    delimiter = ''
+    joined = delimiter.join(text)
+    return joined
+
+
+def del_commas(excerpt):
+   if excerpt is not None:
+    excerpt = excerpt.replace(',',' ')
+
+   return excerpt
+  
+
 def scrap_to_csv(scrap):
     for k,v in scrap.items():
-        line = "{},{},{},{},{},{},{}" \
-                .format(v["title"], v["subtitle"], v["author"],
-                        conform_date(v["time"]), v["section"],
-                        v["text"], k)
+        line = "{0},{1},{2},{3},{4},{5},{6}" \
+                .format(del_commas(v["title"]),
+                        del_commas(v["subtitle"]),
+                        del_commas(v["author"]),
+                        conform_date(v["time"]),
+                        del_commas(v["section"]),
+                        del_commas(conform_text(v["text"])), k)
         return line
 
 
