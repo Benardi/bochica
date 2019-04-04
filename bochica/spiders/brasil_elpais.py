9f99aef862a6fa831b47029c02902c914f7d0ced
Benardi <benardinunes@gmail.com>
2019-04-03 22:36:35 -0300

    Update gitignore. Update crawler to follow links
    Ignore vscode and venv directories.
    Update crawler to follow article pages via link and scrap them.

--- bochica/spiders/brasil_elpais.py
+++ bochica/spiders/brasil_elpais.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
-import scrapy
 import json
+import pdb
+
+import scrapy
+import pandas
 
 from bochica.items import RiLab01Item
 from bochica.items import RiLab01CommentItem
@@ -18,22 +21,26 @@ class BrasilElpaisSpider(scrapy.Spider):
         self.start_urls = list(data.values())
 
     def parse(self, response):
-        #
-        # inclua seu código aqui
-        #
-#        page = response.url.split("/")[-2]
-#        filename = 'quotes-%s.html' % page
-#        with open(filename, 'wb') as f:
-#            f.write(response.body)
-#        self.log('Saved file %s' % filename)
-        for article in response.css("div.articulo__interior"):
-            yield {
-                'url': article.css("h2.articulo-titulo a::attr(href)").get(),             
-                'title': article.css("h2.articulo-titulo a::text").get(),
-                'time': article.css("div.articulo-metadatos time::attr(datetime)").get(),
-                'author': article.css("div.articulo-metadatos a::text").get(),
-                'subtitle': article.css("p.articulo-entradilla::text").get() 	
-         }
-        #
-        #
-        #
+        articles = response.css("div.articulo__interior")
+     
+        for article in articles:
+            url = article.css("h2.articulo-titulo a::attr(href)").get()
+            if url is not None:
+                url = "https:" + url 
+                yield response.follow(url, callback=self.parse_article)
+
+
+        
+    def parse_article(self, response):
+        url = response.request.url
+        page_index = {}
+        page_index[url] = {}
+
+        page_index[url]["section"] = response.css("div.seccion-migas span span::text").get()
+        page_index[url]["title"] = response.css("h1::text").get()
+        page_index[url]["subtitle"] = response.css("h2::text").get()
+        page_index[url]["time"] = response.css("time::attr(datetime)").get()
+        page_index[url]["author"] = response.css("span.autor-nombre a::text").get()
+        page_index[url]["text"] = response.css("div.articulo-cuerpo p::text").getall()
+    
+        return page_index
