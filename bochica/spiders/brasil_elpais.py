1e4fd4faeb0e1a6319fbe3ab4e69253b8ec9f129
Benardi <benardinunes@gmail.com>
2019-04-05 00:27:24 -0300

    Make crawler's most generic functions top-module
    Make parsing subroutines top-module to be re-used by multiple crawlers.

--- bochica/spiders/brasil_elpais.py
+++ bochica/spiders/brasil_elpais.py
@@ -9,6 +9,46 @@ from bochica.items import ArticleItem
 from bochica.items import ArticleCommentItem
 
 
+
+def parse_article(response):
+    
+    item = ArticleItem()
+    item['url'] = response.request.url
+    item["section"] = response.css("div.seccion-migas span span::text").get()
+    item["title"] = response.css("h1::text").get()
+    item["subtitle"] = response.css("h2::text").get()
+    item["date"] = response.css("time::attr(datetime)").get()
+    item["author"] = response.css("span.autor-nombre a::text").get()
+    item["text"] = locate_text(response)
+
+    return item 
+
+def locate_text(response):
+   text = '' 
+   p_res = response.css("div.articulo-cuerpo p::text").getall()
+   p_res_text = ''.join(p_res) 
+
+   span_res = response.css("div.articulo-cuerpo span::text").getall()
+   span_res_text = ''.join(span_res) 
+   
+   galeria_res = response.css("div.articulo-galeria figcaption \
+                              span.foto-texto::text").getall()
+   galeria_res_text = ''.join(galeria_res)
+
+   if len(p_res_text) >= len(span_res_text) and \
+      len(p_res_text) >= len(galeria_res_text):
+        text = p_res
+   
+   elif len(span_res_text) >= len(p_res_text) and \
+      len(span_res_text) >= len(galeria_res_text):
+        text = span_res
+
+   else:
+        text = galeria_res
+   
+   return text
+
+
 class BrasilElpaisSpider(scrapy.Spider):
     name = 'brasil_elpais'
     allowed_domains = ['brasil.elpais.com']
@@ -27,43 +67,6 @@ class BrasilElpaisSpider(scrapy.Spider):
             url = article.css("h2.articulo-titulo a::attr(href)").get()
             if url :
                 url = "https:" + url 
-                yield response.follow(url, callback=self.parse_article)
+                yield response.follow(url, callback=parse_article)
    
-    def parse_article(self, response):
-        
-        item = ArticleItem()
-        item['url'] = response.request.url
-        item["section"] = response.css("div.seccion-migas span span::text").get()
-        item["title"] = response.css("h1::text").get()
-        item["subtitle"] = response.css("h2::text").get()
-        item["date"] = response.css("time::attr(datetime)").get()
-        item["author"] = response.css("span.autor-nombre a::text").get()
-        item["text"] = self.locate_text(response)
-    
-        return item 
-
-    def locate_text(self, response):
-       text = '' 
-       p_res = response.css("div.articulo-cuerpo p::text").getall()
-       p_res_text = ''.join(p_res) 
-
-       span_res = response.css("div.articulo-cuerpo span::text").getall()
-       span_res_text = ''.join(span_res) 
-       
-       galeria_res = response.css("div.articulo-galeria figcaption \
-                                  span.foto-texto::text").getall()
-       galeria_res_text = ''.join(galeria_res)
-
-       if len(p_res_text) >= len(span_res_text) and \
-          len(p_res_text) >= len(galeria_res_text):
-            text = p_res
-       
-       elif len(span_res_text) >= len(p_res_text) and \
-          len(span_res_text) >= len(galeria_res_text):
-            text = span_res
-
-       else:
-            text = galeria_res
-       
-       return text
 
