tiago-clementino/ri_lab_01

Rename scrapy item. Integrate item to pipeline. Edit utils to user argv
Rename scrapy item from RiLab01 to Article.
Replace custom dict in favor of scrapy item within pipeline.
Edit utility code to use sys args to receive data  orig/dest path.

ri_lab_01/pipelines.py
@@ -6,9 +6,9 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from scrapy.conf import settings
 
-from ri_lab_01.items import RiLab01Item
-from ri_lab_01.items import RiLab01CommentItem
+from ri_lab_01.items import ArticleItem
+from ri_lab_01.items import ArticleCommentItem
 
-class RiLab01Pipeline(object):
+class ArticlePipeline(object):
     def process_item(self, item, spider):
         return item

ri_lab_01/settings.py
@@ -62,7 +62,7 @@ ROBOTSTXT_OBEY = True
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    'ri_lab_01.middlewares.RiLab01SpiderMiddleware': 543,
+#    'ri_lab_01.middlewares.ArticleSpiderMiddleware': 543,
 #}
 
 DOWNLOADER_MIDDLEWARES = {
@@ -85,7 +85,7 @@ ROTATING_PROXY_LIST = [
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'ri_lab_01.middlewares.RiLab01DownloaderMiddleware': 543,
+#    'ri_lab_01.middlewares.ArticleDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
@@ -97,10 +97,10 @@ ROTATING_PROXY_LIST = [
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
-#    'ri_lab_01.pipelines.RiLab01Pipeline': 300,
+#    'ri_lab_01.pipelines.ArticlePipeline': 300,
 #}
 ITEM_PIPELINES = {
-   'ri_lab_01.pipelines.RiLab01Pipeline': 300,
+   'ri_lab_01.pipelines.ArticlePipeline': 300,
 }
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html

ri_lab_01/spiders/brasil_247.py
@@ -2,8 +2,8 @@
 import scrapy
 import json
 
-from ri_lab_01.items import RiLab01Item
-from ri_lab_01.items import RiLab01CommentItem
+from ri_lab_01.items import ArticleItem
+from ri_lab_01.items import ArticleCommentItem
 
 
 class Brasil247Spider(scrapy.Spider):

ri_lab_01/spiders/brasil_elpais.py
@@ -5,8 +5,8 @@ import pdb
 import scrapy
 import pandas
 
-from ri_lab_01.items import RiLab01Item
-from ri_lab_01.items import RiLab01CommentItem
+from ri_lab_01.items import ArticleItem
+from ri_lab_01.items import ArticleCommentItem
 
 
 class BrasilElpaisSpider(scrapy.Spider):
@@ -28,22 +28,19 @@ class BrasilElpaisSpider(scrapy.Spider):
             if url :
                 url = "https:" + url 
                 yield response.follow(url, callback=self.parse_article)
-
-
-        
+   
     def parse_article(self, response):
-        url = response.request.url
-        page_index = {}
-        page_index[url] = {}
-
-        page_index[url]["section"] = response.css("div.seccion-migas span span::text").get()
-        page_index[url]["title"] = response.css("h1::text").get()
-        page_index[url]["subtitle"] = response.css("h2::text").get()
-        page_index[url]["time"] = response.css("time::attr(datetime)").get()
-        page_index[url]["author"] = response.css("span.autor-nombre a::text").get()
-        page_index[url]["text"] = self.locate_text(response)
+        
+        item = ArticleItem()
+        item['url'] = response.request.url
+        item["section"] = response.css("div.seccion-migas span span::text").get()
+        item["title"] = response.css("h1::text").get()
+        item["subtitle"] = response.css("h2::text").get()
+        item["date"] = response.css("time::attr(datetime)").get()
+        item["author"] = response.css("span.autor-nombre a::text").get()
+        item["text"] = self.locate_text(response)
     
-        return page_index
+        return item 
 
     def locate_text(self, response):
        text = '' 

ri_lab_01/spiders/carta_capital.py
@@ -2,8 +2,8 @@
 import scrapy
 import json
 
-from ri_lab_01.items import RiLab01Item
-from ri_lab_01.items import RiLab01CommentItem
+from ri_lab_01.items import ArticleItem
+from ri_lab_01.items import ArticleCommentItem
 
 
 class CartaCapitalSpider(scrapy.Spider):

ri_lab_01/spiders/diario_do_centro_do_mundo.py
@@ -2,8 +2,8 @@
 import scrapy
 import json
 
-from ri_lab_01.items import RiLab01Item
-from ri_lab_01.items import RiLab01CommentItem
+from ri_lab_01.items import ArticleItem
+from ri_lab_01.items import ArticleCommentItem
 
 
 class DiarioDoCentroDoMundoSpider(scrapy.Spider):

ri_lab_01/spiders/gazeta_do_povo.py
@@ -2,8 +2,8 @@
 import scrapy
 import json
 
-from ri_lab_01.items import RiLab01Item
-from ri_lab_01.items import RiLab01CommentItem
+from ri_lab_01.items import ArticleItem
+from ri_lab_01.items import ArticleCommentItem
 
 
 class GazetaDoPovoSpider(scrapy.Spider):

ri_lab_01/spiders/oantagonista.py
@@ -2,8 +2,8 @@
 import scrapy
 import json
 
-from ri_lab_01.items import RiLab01Item
-from ri_lab_01.items import RiLab01CommentItem
+from ri_lab_01.items import ArticleItem
+from ri_lab_01.items import ArticleCommentItem
 
 
 class OantagonistaSpider(scrapy.Spider):

Benardi/bochica

bochica/pipelines.py
@@ -6,9 +6,9 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from scrapy.conf import settings
 
-from bochica.items import RiLab01Item
-from bochica.items import RiLab01CommentItem
+from bochica.items import ArticleItem
+from bochica.items import ArticleCommentItem
 
-class RiLab01Pipeline(object):
+class ArticlePipeline(object):
     def process_item(self, item, spider):
         return item

bochica/settings.py
@@ -62,7 +62,7 @@ ROBOTSTXT_OBEY = True
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    'bochica.middlewares.RiLab01SpiderMiddleware': 543,
+#    'bochica.middlewares.ArticleSpiderMiddleware': 543,
 #}
 
 DOWNLOADER_MIDDLEWARES = {
@@ -85,7 +85,7 @@ ROTATING_PROXY_LIST = [
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'bochica.middlewares.RiLab01DownloaderMiddleware': 543,
+#    'bochica.middlewares.ArticleDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
@@ -97,10 +97,10 @@ ROTATING_PROXY_LIST = [
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
-#    'bochica.pipelines.RiLab01Pipeline': 300,
+#    'bochica.pipelines.ArticlePipeline': 300,
 #}
 ITEM_PIPELINES = {
-   'bochica.pipelines.RiLab01Pipeline': 300,
+   'bochica.pipelines.ArticlePipeline': 300,
 }
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html

bochica/spiders/brasil_247.py
@@ -2,8 +2,8 @@
 import scrapy
 import json
 
-from bochica.items import RiLab01Item
-from bochica.items import RiLab01CommentItem
+from bochica.items import ArticleItem
+from bochica.items import ArticleCommentItem
 
 
 class Brasil247Spider(scrapy.Spider):

bochica/spiders/brasil_elpais.py
@@ -5,8 +5,8 @@ import pdb
 import scrapy
 import pandas
 
-from bochica.items import RiLab01Item
-from bochica.items import RiLab01CommentItem
+from bochica.items import ArticleItem
+from bochica.items import ArticleCommentItem
 
 
 class BrasilElpaisSpider(scrapy.Spider):
@@ -28,22 +28,19 @@ class BrasilElpaisSpider(scrapy.Spider):
             if url :
                 url = "https:" + url 
                 yield response.follow(url, callback=self.parse_article)
-
-
-        
+   
     def parse_article(self, response):
-        url = response.request.url
-        page_index = {}
-        page_index[url] = {}
-
-        page_index[url]["section"] = response.css("div.seccion-migas span span::text").get()
-        page_index[url]["title"] = response.css("h1::text").get()
-        page_index[url]["subtitle"] = response.css("h2::text").get()
-        page_index[url]["time"] = response.css("time::attr(datetime)").get()
-        page_index[url]["author"] = response.css("span.autor-nombre a::text").get()
-        page_index[url]["text"] = self.locate_text(response)
+        
+        item = ArticleItem()
+        item['url'] = response.request.url
+        item["section"] = response.css("div.seccion-migas span span::text").get()
+        item["title"] = response.css("h1::text").get()
+        item["subtitle"] = response.css("h2::text").get()
+        item["date"] = response.css("time::attr(datetime)").get()
+        item["author"] = response.css("span.autor-nombre a::text").get()
+        item["text"] = self.locate_text(response)
     
-        return page_index
+        return item 
 
     def locate_text(self, response):
        text = '' 

bochica/spiders/carta_capital.py
@@ -2,8 +2,8 @@
 import scrapy
 import json
 
-from bochica.items import RiLab01Item
-from bochica.items import RiLab01CommentItem
+from bochica.items import ArticleItem
+from bochica.items import ArticleCommentItem
 
 
 class CartaCapitalSpider(scrapy.Spider):

bochica/spiders/diario_do_centro_do_mundo.py
@@ -2,8 +2,8 @@
 import scrapy
 import json
 
-from bochica.items import RiLab01Item
-from bochica.items import RiLab01CommentItem
+from bochica.items import ArticleItem
+from bochica.items import ArticleCommentItem
 
 
 class DiarioDoCentroDoMundoSpider(scrapy.Spider):

bochica/spiders/gazeta_do_povo.py
@@ -2,8 +2,8 @@
 import scrapy
 import json
 
-from bochica.items import RiLab01Item
-from bochica.items import RiLab01CommentItem
+from bochica.items import ArticleItem
+from bochica.items import ArticleCommentItem
 
 
 class GazetaDoPovoSpider(scrapy.Spider):

bochica/spiders/oantagonista.py
@@ -2,8 +2,8 @@
 import scrapy
 import json
 
-from bochica.items import RiLab01Item
-from bochica.items import RiLab01CommentItem
+from bochica.items import ArticleItem
+from bochica.items import ArticleCommentItem
 
 
 class OantagonistaSpider(scrapy.Spider):