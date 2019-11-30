yurikelvin/ri_lab_01

First commit with all code

ri_lab_01/pipelines.py
@@ -1,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from pymongo import MongoClient
+from scrapy.conf import settings
+
+from ri_lab_01.items import RiLab01Item
+from ri_lab_01.items import RiLab01CommentItem
+
+class RiLab01Pipeline(object):
+    client = None;
+    db = None;
+
+    def open_spider(self, spider):
+        # init client mongo
+        self.client = MongoClient(settings['MONGODB_SERVER'], settings['MONGODB_PORT'])
+        # set database
+        self.db = self.client[settings['MONGODB_DB']]
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        if isinstance(item, RiLab01Item):
+            self.db[spider.name].insert_one(item)
+        elif isinstance(item, RiLab01CommentItem):
+            self.db[spider.name + 'Comments'].insert_one(dict(item))
+        else:
+            self.db[spider.name + 'MetaData'].insert_one(item)
+        return item

ri_lab_01/settings.py
@@ -1,0 +1,128 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for ri_lab_01 project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://doc.scrapy.org/en/latest/topics/settings.html
+#     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = 'ri_lab_01'
+
+SPIDER_MODULES = ['ri_lab_01.spiders']
+NEWSPIDER_MODULE = 'ri_lab_01.spiders'
+
+DEADLINE = "01.01.2018" # DD.MM.YYYY
+
+MONGODB_SERVER = "localhost"
+MONGODB_PORT = 27017
+MONGODB_DB = "news"
+
+USER_AGENTS = [
+    ('Mozilla/5.0 (X11; Linux x86_64) '
+     'AppleWebKit/537.36 (KHTML, like Gecko) '
+     'Chrome/57.0.2987.110 '
+     'Safari/537.36'),  # chrome
+    ('Mozilla/5.0 (X11; Linux x86_64) '
+     'AppleWebKit/537.36 (KHTML, like Gecko) '
+     'Chrome/61.0.3163.79 '
+     'Safari/537.36'),  # chrome
+    ('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:55.0) '
+     'Gecko/20100101 '
+     'Firefox/55.0')  # firefox
+]
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'ri_lab_01 (+http://www.yourdomain.com)'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = True
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+#CONCURRENT_REQUESTS = 32
+
+# Configure a delay for requests for the same website (default: 0)
+# See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+#DOWNLOAD_DELAY = 3
+# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
+
+# Disable cookies (enabled by default)
+#COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+#DEFAULT_REQUEST_HEADERS = {
+#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#   'Accept-Language': 'en',
+#}
+
+# Enable or disable spider middlewares
+# See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    'ri_lab_01.middlewares.RiLab01SpiderMiddleware': 543,
+#}
+
+DOWNLOADER_MIDDLEWARES = {
+   # USER_AGENTS
+   'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+   'scrapy_useragents.downloadermiddlewares.useragents.UserAgentsMiddleware': 500,
+   # ROTATING_PROXY_LIST
+   'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
+   'rotating_proxies.middlewares.BanDetectionMiddleware': 620,
+}
+
+# Define used proxies
+ROTATING_PROXY_LIST = [
+  '189.51.153.195:57898', '45.162.37.79:8080',   '179.108.187.9:8080',
+  '177.23.104.38:61536',  '187.32.123.177:3128', '45.71.80.34:37122',
+  '45.162.37.79:8080',    '186.250.55.233:50741','179.108.187.9:8080',
+  '187.32.123.177:3128',  '187.32.123.177:3128', '177.72.92.24:23500'
+]
+
+# Enable or disable downloader middlewares
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+#DOWNLOADER_MIDDLEWARES = {
+#    'ri_lab_01.middlewares.RiLab01DownloaderMiddleware': 543,
+#}
+
+# Enable or disable extensions
+# See https://doc.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    'scrapy.extensions.telnet.TelnetConsole': None,
+#}
+
+# Configure item pipelines
+# See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+#ITEM_PIPELINES = {
+#    'ri_lab_01.pipelines.RiLab01Pipeline': 300,
+#}
+ITEM_PIPELINES = {
+   'ri_lab_01.pipelines.RiLab01Pipeline': 300,
+}
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = 'httpcache'
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'

ri_lab_01/spiders/brasil_247.py
@@ -1,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+
+from ri_lab_01.items import RiLab01Item
+from ri_lab_01.items import RiLab01CommentItem
+
+
+class Brasil247Spider(scrapy.Spider):
+    name = 'brasil_247'
+    allowed_domains = ['brasil247.com']
+    start_urls = []
+
+    def __init__(self, *a, **kw):
+        super(Brasil247Spider, self).__init__(*a, **kw)
+        with open('frontier/brasil_247.json') as json_file:
+                data = json.load(json_file)
+        self.start_urls = list(data.values())
+
+    def parse(self, response):
+        #
+        # inclua seu código aqui
+        #
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
+        #
+        #
+        #

ri_lab_01/spiders/brasil_elpais.py
@@ -1,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+
+from ri_lab_01.items import RiLab01Item
+from ri_lab_01.items import RiLab01CommentItem
+
+
+class BrasilElpaisSpider(scrapy.Spider):
+    name = 'brasil_elpais'
+    allowed_domains = ['brasil.elpais.com']
+    start_urls = []
+
+    def __init__(self, *a, **kw):
+        super(BrasilElpaisSpider, self).__init__(*a, **kw)
+        with open('frontier/brasil_elpais.json') as json_file:
+                data = json.load(json_file)
+        self.start_urls = list(data.values())
+
+    def parse(self, response):
+        #
+        # inclua seu código aqui
+        #
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
+        #
+        #
+        #

ri_lab_01/spiders/carta_capital.py
@@ -1,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+
+from ri_lab_01.items import RiLab01Item
+from ri_lab_01.items import RiLab01CommentItem
+
+
+class CartaCapitalSpider(scrapy.Spider):
+    name = 'carta_capital'
+    allowed_domains = ['cartacapital.com.br']
+    start_urls = []
+
+    def __init__(self, *a, **kw):
+        super(CartaCapitalSpider, self).__init__(*a, **kw)
+        with open('frontier/carta_capital.json') as json_file:
+                data = json.load(json_file)
+        self.start_urls = list(data.values())
+
+    def parse(self, response):
+        #
+        # inclua seu código aqui
+        #
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
+        #
+        #
+        #

ri_lab_01/spiders/diario_do_centro_do_mundo.py
@@ -1,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+
+from ri_lab_01.items import RiLab01Item
+from ri_lab_01.items import RiLab01CommentItem
+
+
+class DiarioDoCentroDoMundoSpider(scrapy.Spider):
+    name = 'diario_do_centro_do_mundo'
+    allowed_domains = ['diariodocentrodomundo.com.br']
+    start_urls = []
+
+    def __init__(self, *a, **kw):
+        super(DiarioDoCentroDoMundoSpider, self).__init__(*a, **kw)
+        with open('frontier/diario_do_centro_do_mundo.json') as json_file:
+                data = json.load(json_file)
+        self.start_urls = list(data.values())
+
+    def parse(self, response):
+        #
+        # inclua seu código aqui
+        #
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
+        #
+        #
+        #

ri_lab_01/spiders/gazeta_do_povo.py
@@ -1,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+
+from ri_lab_01.items import RiLab01Item
+from ri_lab_01.items import RiLab01CommentItem
+
+
+class GazetaDoPovoSpider(scrapy.Spider):
+    name = 'gazeta_do_povo'
+    allowed_domains = ['gazetadopovo.com.br']
+    start_urls = []
+
+    def __init__(self, *a, **kw):
+        super(GazetaDoPovoSpider, self).__init__(*a, **kw)
+        with open('frontier/gazeta_do_povo.json') as json_file:
+                data = json.load(json_file)
+        self.start_urls = list(data.values())
+
+    def parse(self, response):
+        #
+        # inclua seu código aqui
+        #
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
+        #
+        #
+        #

ri_lab_01/spiders/oantagonista.py
@@ -1,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+
+from ri_lab_01.items import RiLab01Item
+from ri_lab_01.items import RiLab01CommentItem
+
+
+class OantagonistaSpider(scrapy.Spider):
+    name = 'oantagonista'
+    allowed_domains = ['oantagonista.com']
+    start_urls = []
+
+    def __init__(self, *a, **kw):
+        super(OantagonistaSpider, self).__init__(*a, **kw)
+        with open('frontier/oantagonista.json') as json_file:
+                data = json.load(json_file)
+        self.start_urls = list(data.values())
+
+    def parse(self, response):
+        #
+        # inclua seu código aqui
+        #
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
+        #
+        #
+        #

Benardi/bochica

bochica/pipelines.py
@@ -1,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from pymongo import MongoClient
+from scrapy.conf import settings
+
+from bochica.items import RiLab01Item
+from bochica.items import RiLab01CommentItem
+
+class RiLab01Pipeline(object):
+    client = None;
+    db = None;
+
+    def open_spider(self, spider):
+        # init client mongo
+        self.client = MongoClient(settings['MONGODB_SERVER'], settings['MONGODB_PORT'])
+        # set database
+        self.db = self.client[settings['MONGODB_DB']]
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        if isinstance(item, RiLab01Item):
+            self.db[spider.name].insert_one(item)
+        elif isinstance(item, RiLab01CommentItem):
+            self.db[spider.name + 'Comments'].insert_one(dict(item))
+        else:
+            self.db[spider.name + 'MetaData'].insert_one(item)
+        return item

bochica/settings.py
@@ -1,0 +1,128 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for bochica project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://doc.scrapy.org/en/latest/topics/settings.html
+#     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = 'bochica'
+
+SPIDER_MODULES = ['bochica.spiders']
+NEWSPIDER_MODULE = 'bochica.spiders'
+
+DEADLINE = "01.01.2018" # DD.MM.YYYY
+
+MONGODB_SERVER = "localhost"
+MONGODB_PORT = 27017
+MONGODB_DB = "news"
+
+USER_AGENTS = [
+    ('Mozilla/5.0 (X11; Linux x86_64) '
+     'AppleWebKit/537.36 (KHTML, like Gecko) '
+     'Chrome/57.0.2987.110 '
+     'Safari/537.36'),  # chrome
+    ('Mozilla/5.0 (X11; Linux x86_64) '
+     'AppleWebKit/537.36 (KHTML, like Gecko) '
+     'Chrome/61.0.3163.79 '
+     'Safari/537.36'),  # chrome
+    ('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:55.0) '
+     'Gecko/20100101 '
+     'Firefox/55.0')  # firefox
+]
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'bochica (+http://www.yourdomain.com)'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = True
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+#CONCURRENT_REQUESTS = 32
+
+# Configure a delay for requests for the same website (default: 0)
+# See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+#DOWNLOAD_DELAY = 3
+# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
+
+# Disable cookies (enabled by default)
+#COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+#DEFAULT_REQUEST_HEADERS = {
+#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#   'Accept-Language': 'en',
+#}
+
+# Enable or disable spider middlewares
+# See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    'bochica.middlewares.RiLab01SpiderMiddleware': 543,
+#}
+
+DOWNLOADER_MIDDLEWARES = {
+   # USER_AGENTS
+   'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+   'scrapy_useragents.downloadermiddlewares.useragents.UserAgentsMiddleware': 500,
+   # ROTATING_PROXY_LIST
+   'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
+   'rotating_proxies.middlewares.BanDetectionMiddleware': 620,
+}
+
+# Define used proxies
+ROTATING_PROXY_LIST = [
+  '189.51.153.195:57898', '45.162.37.79:8080',   '179.108.187.9:8080',
+  '177.23.104.38:61536',  '187.32.123.177:3128', '45.71.80.34:37122',
+  '45.162.37.79:8080',    '186.250.55.233:50741','179.108.187.9:8080',
+  '187.32.123.177:3128',  '187.32.123.177:3128', '177.72.92.24:23500'
+]
+
+# Enable or disable downloader middlewares
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+#DOWNLOADER_MIDDLEWARES = {
+#    'bochica.middlewares.RiLab01DownloaderMiddleware': 543,
+#}
+
+# Enable or disable extensions
+# See https://doc.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    'scrapy.extensions.telnet.TelnetConsole': None,
+#}
+
+# Configure item pipelines
+# See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+#ITEM_PIPELINES = {
+#    'bochica.pipelines.RiLab01Pipeline': 300,
+#}
+ITEM_PIPELINES = {
+   'bochica.pipelines.RiLab01Pipeline': 300,
+}
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = 'httpcache'
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'

bochica/spiders/brasil_247.py
@@ -1,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+
+from bochica.items import RiLab01Item
+from bochica.items import RiLab01CommentItem
+
+
+class Brasil247Spider(scrapy.Spider):
+    name = 'brasil_247'
+    allowed_domains = ['brasil247.com']
+    start_urls = []
+
+    def __init__(self, *a, **kw):
+        super(Brasil247Spider, self).__init__(*a, **kw)
+        with open('frontier/brasil247.json') as json_file:
+                data = json.load(json_file)
+        self.start_urls = list(data.values())
+
+    def parse(self, response):
+        #
+        # inclua seu código aqui
+        #
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
+        #
+        #
+        #

bochica/spiders/brasil_elpais.py
@@ -1,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+
+from bochica.items import RiLab01Item
+from bochica.items import RiLab01CommentItem
+
+
+class BrasilElpaisSpider(scrapy.Spider):
+    name = 'brasil_elpais'
+    allowed_domains = ['brasil.elpais.com']
+    start_urls = []
+
+    def __init__(self, *a, **kw):
+        super(BrasilElpaisSpider, self).__init__(*a, **kw)
+        with open('frontier/brasil_elpais.json') as json_file:
+                data = json.load(json_file)
+        self.start_urls = list(data.values())
+
+    def parse(self, response):
+        #
+        # inclua seu código aqui
+        #
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
+        #
+        #
+        #

bochica/spiders/carta_capital.py
@@ -1,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+
+from bochica.items import RiLab01Item
+from bochica.items import RiLab01CommentItem
+
+
+class CartaCapitalSpider(scrapy.Spider):
+    name = 'carta_capital'
+    allowed_domains = ['cartacapital.com.br']
+    start_urls = []
+
+    def __init__(self, *a, **kw):
+        super(CartaCapitalSpider, self).__init__(*a, **kw)
+        with open('frontier/carta_capital.json') as json_file:
+                data = json.load(json_file)
+        self.start_urls = list(data.values())
+
+    def parse(self, response):
+        #
+        # inclua seu código aqui
+        #
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
+        #
+        #
+        #

bochica/spiders/diario_do_centro_do_mundo.py
@@ -1,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+
+from bochica.items import RiLab01Item
+from bochica.items import RiLab01CommentItem
+
+
+class DiarioDoCentroDoMundoSpider(scrapy.Spider):
+    name = 'diario_do_centro_do_mundo'
+    allowed_domains = ['diariodocentrodomundo.com.br']
+    start_urls = []
+
+    def __init__(self, *a, **kw):
+        super(DiarioDoCentroDoMundoSpider, self).__init__(*a, **kw)
+        with open('frontier/diariodocentrodomundo.json') as json_file:
+                data = json.load(json_file)
+        self.start_urls = list(data.values())
+
+    def parse(self, response):
+        #
+        # inclua seu código aqui
+        #
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
+        #
+        #
+        #

bochica/spiders/gazeta_do_povo.py
@@ -1,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+
+from bochica.items import RiLab01Item
+from bochica.items import RiLab01CommentItem
+
+
+class GazetaDoPovoSpider(scrapy.Spider):
+    name = 'gazeta_do_povo'
+    allowed_domains = ['gazetadopovo.com.br']
+    start_urls = []
+
+    def __init__(self, *a, **kw):
+        super(GazetaDoPovoSpider, self).__init__(*a, **kw)
+        with open('frontier/gazetadopovo.json') as json_file:
+                data = json.load(json_file)
+        self.start_urls = list(data.values())
+
+    def parse(self, response):
+        #
+        # inclua seu código aqui
+        #
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
+        #
+        #
+        #

bochica/spiders/oantagonista.py
@@ -1,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+
+from bochica.items import RiLab01Item
+from bochica.items import RiLab01CommentItem
+
+
+class OantagonistaSpider(scrapy.Spider):
+    name = 'oantagonista'
+    allowed_domains = ['oantagonista.com']
+    start_urls = []
+
+    def __init__(self, *a, **kw):
+        super(OantagonistaSpider, self).__init__(*a, **kw)
+        with open('frontier/oantagonista.json') as json_file:
+                data = json.load(json_file)
+        self.start_urls = list(data.values())
+
+    def parse(self, response):
+        #
+        # inclua seu código aqui
+        #
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
+        #
+        #
+        #